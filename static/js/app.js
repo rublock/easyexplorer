11edc69323e2648d967d3f766f4689519a552cd6
hypermail <hypermail@yandex.ru>
2023-01-13 12:59:52 +0300

    main usd balance js done

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,13 @@
-console.log('test js')
+let balanceToggle = document.querySelector('.balanceToggle');
+let btcBalance = document.querySelector('.btcBalance');
+let usdBalance = document.querySelector('.usdBalance');
+
+balanceToggle.addEventListener("click", () => {
+    if (usdBalance.classList.contains('hide')) {
+        btcBalance.classList.toggle('hide')
+        usdBalance.classList.toggle('hide')
+    } else {
+        btcBalance.classList.toggle('hide')
+        usdBalance.classList.toggle('hide')
+    }
+})
