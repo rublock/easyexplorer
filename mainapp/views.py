e4576b224f05734982b20b66c33ca585639fd96f
hypermail <hypermail@yandex.ru>
2023-01-28 14:48:19 +0300

    main new API done, JS pagination done

--- mainapp/views.py
+++ mainapp/views.py
@@ -14,71 +14,53 @@ class MainPageView(TemplateView):
 class AddressView(View):
     def get(self, request):
 
-        blockchair_API = requests.get('https://api.blockchair.com/bitcoin/stats')
+        user_search = request.GET.get("address")
+
+        blockchair_API = requests.get("https://api.blockchair.com/bitcoin/stats")
         blockchair_data = json.loads(blockchair_API.content)
 
         error = False
 
         try:
-            address_details = get_address_details(
-                # get data from <input name="address" /> base.html
-                request.GET.get("address"), txn_limit=9999
+            """
+            https://blockchain.info/rawaddr/$bitcoin_address
+            Address can be base58 or hash160
+            Optional limit parameter to show n transactions e.g. &limit=50 (Default: 50, Max: 50)
+            Optional offset parameter to skip the first n transactions e.g. &offset=100 (Page 2 for limit 50)
+            """
+            get_api_data = requests.get(
+                f"https://blockchain.info/rawaddr/{user_search}?limit=9999"
             )
+            get_content = get_api_data._content
+            single_address_data = json.loads(get_content)
         except (AssertionError, IndexError):
             error = True
 
         if error == True:
-            return render(request, 'mainapp/base_error.html')
+            return render(request, "mainapp/base_error.html")
         else:
-            temp_list = []
-            temp_array = []
-            dict = {}
-            count = 0
-            balance = address_details["balance"] / 100000000
-
             qr = qrcode.QRCode(
                 box_size=10,
                 border=6,
             )
-            qr.add_data(address_details["address"])
-
+            qr.add_data(single_address_data["address"])
             img = qr.make_image(fill_color="#F7931A", back_color="white")
-
             img.save("static/img/qr.png")
 
-            for i in address_details["txrefs"]:
-                if i["tx_hash"] in temp_list:
-                    temp_array[temp_list.index(i["tx_hash"])]["value"] += i["value"]
-                else:
-                    temp_list.append(i["tx_hash"])
-                    temp_array.append(i)
+            address = single_address_data["address"]
+            n_tx = single_address_data["n_tx"]
+            balance = single_address_data["final_balance"] / 100000000
 
-            for i in temp_array:
-                temp_dict = {}
-                if i["tx_input_n"] >= 0:
-                    minus_value = 0 - i["value"]
-                    temp_dict[0] = i["tx_hash"]
-                    temp_dict[1] = i["confirmed"].strftime("%d.%m.%Y %H:%M")
-                    temp_dict[2] = minus_value / 100000000
-                    dict[count] = temp_dict
-                    count += 1
-                else:
-                    temp_dict[0] = i["tx_hash"]
-                    temp_dict[1] = i["confirmed"].strftime("%d.%m.%Y %H:%M")
-                    temp_dict[2] = i["value"] / 100000000
-                    dict[count] = temp_dict
-                    count += 1
-
-            tx_data_json = json.dumps(dict)
+            tx_data_json = json.dumps(single_address_data["txs"])
 
             return render(
                 request,
                 "mainapp/address.html",
                 {
-                    "address_details": address_details,
+                    "address": address,
                     "balance": balance,
+                    "n_tx": n_tx,
                     "tx_data_json": tx_data_json,
                     "blockchair_data": blockchair_data,
                 },
             )
-
--- static/js/app.js
+++ static/js/app.js
@@ -2,12 +2,16 @@ let balanceToggle = document.querySelector('.balanceToggle');
 let btcBalance = document.querySelector('.btcBalance');
 let usdBalance = document.querySelector('.usdBalance');
 
-balanceToggle.addEventListener("click", () => {
-    if (usdBalance.classList.contains('hide')) {
-        btcBalance.classList.toggle('hide')
-        usdBalance.classList.toggle('hide')
-    } else {
-        btcBalance.classList.toggle('hide')
-        usdBalance.classList.toggle('hide')
-    }
-})
+try {
+    balanceToggle.addEventListener("click", () => {
+        if (usdBalance.classList.contains('hide')) {
+            btcBalance.classList.toggle('hide')
+            usdBalance.classList.toggle('hide')
+        } else {
+            btcBalance.classList.toggle('hide')
+            usdBalance.classList.toggle('hide')
+        }
+    })
+} catch {
+    console.log('Welcome to btcblockexplorer.io!')
+}
