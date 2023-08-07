ed86088ec76350d163be7b18172840a4023e1971
mack s1 <admin@1828773-ce63256.twc1.net>
2023-08-07 20:14:38 +0300

    req.txt allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-f@z=6f@eap4-73c_xcnh7)_*jhs38y_a_e+==-lud05&o4d9_q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['185.192.247.249', 'easyexplorer.io']
 
 
 # Application definition
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -130,5 +130,5 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / '/static/',
 ]
